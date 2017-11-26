93dbcf3fe89e730c72cca58f7ae1eab768a7a5b4
Sébastien Valat <sebastien.valat@gmail.com>
2017-11-26 11:47:40 +0100

    first commit

---
+++ js/menu.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	//define
+	var menuEntries = [
+		{title: 'Home', url: 'index.html'},
+		{title: 'Research', url: 'research.html'},
+		{title: 'Projects',url: 'projects.html'},
+		{title: 'Timeline',url: 'timeline.html'},
+		{title: 'CV', url: 'cv.html'},
+		//{title: 'Contribute', url: 'contrib.html'},
+	];
+	
+	//get current address
+	var pathname = $(location).attr('pathname').split('/').pop();
+	
+	//setup
+	var ul = $(".navbar ul");
+	
+	//insert
+	for (var i in menuEntries)
+	{
+		//create link
+		var a = $("<a>")
+			.text(menuEntries[i].title)
+			.attr('href',menuEntries[i].url)
+		
+		//if current
+		if (pathname == menuEntries[i].url)
+			a.addClass('active');
+
+		//append
+		ul.append($("<li>").append(a));
+	}
+});
